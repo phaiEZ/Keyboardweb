ee39953349d587de289cfcb69fefc7fe7d3f726a
Monk7 <63010332@kmitl.ac.th>
2022-05-05 10:08:40 +0700

    new

--- src/dummies/userinfo.jsx
+++
@@ -1,8 +0,0 @@
-const UserInfo = 
-    {
-        'img' : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png',
-        'Name' : 'Chayut phunpet',
-        'Detail' : "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam expedita nobis facere eos consequatur saepe iure iste at voluptatibus voluptatem beatae, incidunt optio nemo enim dicta distinctio et corporis reiciendis?",
-    }
-
-export default UserInfo
